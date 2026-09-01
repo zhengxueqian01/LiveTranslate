c17881ebcf3e8a0a5a5afdc3f1382f071506179e
xueqianzheng <1029776761@qq.com>
2026-09-01 17:26:17 +0800

    chore: scaffold LiveTranslate iOS project

---
+++ ios/LiveTranslate/LiveTranslate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiveTranslate
+//
+//  Created by Xueqian Zheng on 2026/9/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/LiveTranslate/LiveTranslate/LiveTranslateApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveTranslateApp.swift
+//  LiveTranslate
+//
+//  Created by Xueqian Zheng on 2026/9/1.
+//
+
+import SwiftUI
+
+@main
+struct LiveTranslateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
